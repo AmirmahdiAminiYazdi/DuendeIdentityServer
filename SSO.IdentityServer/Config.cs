44fdb5a5e0a407986d58dd7047fd49cb4429592b
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2023-11-21 17:15:29 +0330

    initial provider

---
+++ SSO.IdentityServer/Config.cs
@@ -0,0 +1,31 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace SSO.IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        { 
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+       new List<ApiScope> { new ApiScope(name:"API1",displayName: "WeatherForecast") };
+
+    public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId ="client",
+                    AllowedGrantTypes =GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes ={ "API1" }
+
+                }
+            };
+}
