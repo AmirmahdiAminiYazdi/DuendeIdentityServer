821b1cb739dd787f7d85140f2cfbb7545d2482cc
amir mahdi amini yazdi <amirmahdi.amini@yahoo.com>
2023-11-21 16:05:36 +0330

    initial Project

--- SSO.Blazor/Program.cs
+++ SSO.Blazor/Program.cs
@@ -5,7 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddHttpClient("W",
+    x => { x.BaseAddress = new Uri("https://localhost:7001"); });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
